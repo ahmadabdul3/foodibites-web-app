48ddaefc35027093c984818dc8b3e58298ed933c
Mahmud <ahmad.mahmud1997@gmail.com>
2019-06-23 21:37:42 -0400

    Author: Mahmud Ahmad ./frontend/pages/home_page.js     changed filtering on product status from a FormInput     into check boxes
        changed the functionality of the FormInput to search
        by product brands... just to keep it in.
    
        -- TODO --
            checkboxes each have their own function right now
            need to create a component that passes value, name
            to the onChange function...
    
            functionality of FormInput needs to search by product
            and not just product.brand... e.g. if the name of the
            product includes, if the brqnd includes, etc...

--- frontend/pages/home_page.js
+++ frontend/pages/home_page.js
@@ -4,6 +4,8 @@ import appRoutes from 'src/constants/routes';
 import http from 'src/frontend/services/http';
 import FormInput from 'src/frontend/components/form_input';
 import ProductSummary from 'src/frontend/components/product_summary';
+import { throwStatement } from 'babel-types';
+import { productStatusComplete, productStatusIncomplete, productStatusPendingReview } from 'src/constants/product_status';
 
 export default class HomePage extends Component {
   state = {
@@ -11,7 +13,10 @@ export default class HomePage extends Component {
     error: '',
     redirectTo: '',
     products: '',
-    dataInputStatusFilterString: '',
+    brandFilterString: '',
+    complete: '',
+    incomplete: '',
+    pendingReview: '',
   }
 
   goToProductNewPage = () => {
@@ -30,16 +35,40 @@ export default class HomePage extends Component {
     this.fetchProducts();
   }
 
-  stringIncludes = (string, include) => {
-    return string.toLowerCase().includes(include.toLowerCase());
+  productStatusIncludesFilter = (string) => {
+    const { complete, incomplete, pendingReview } = this.state;
+    string = string.toLowerCase();
+    let ret = false;
+    let filtExists = false;
+    if (this.state.complete) {
+      ret |= string === complete.toLowerCase();
+      filtExists = true;
+    }
+    if (this.state.incomplete) {
+      ret |= string === incomplete.toLowerCase();
+      filtExists = true;
+    }
+    if (this.state.pendingReview) {
+      ret |= string === pendingReview.toLowerCase();
+      filtExists = true;
+    }
+    if (!filtExists) {
+      return true;
+    }
+    return ret;
+  }
+
+  productBrandIncludesFilter = (string) => {
+    const { brandFilterString } = this.state;
+    return string.toLowerCase().includes(brandFilterString.toLowerCase());
   }
 
   renderProducts = () => {
-    const { products, dataInputStatusFilterString } = this.state;
+    const { products, brandFilterString } = this.state;
     if (!products) return <div> No products </div>;
     const productsMarkup = products.map((datum, i) => {
-      if (dataInputStatusFilterString && !this.stringIncludes(datum.dataInputStatus, dataInputStatusFilterString)) return;
-
+      if (!this.productStatusIncludesFilter(datum.dataInputStatus)) return;
+      if (brandFilterString && !this.productBrandIncludesFilter(datum.brand)) return;
       return (
         <ProductSummary
           product={datum}
@@ -59,6 +88,37 @@ export default class HomePage extends Component {
     this.setState({ [name]: value })
   }
 
+  checkBoxOnChangeComplete = ({ name, value }) => {
+    // console.log('checkbox');
+    // console.log('name & value', name, value)
+    // if(this.state[name]) {
+    //   this.setState({ [name]: '' });
+    // } else {
+    //   this.setState({ [name]: value });
+    // }
+    if(this.state.complete) {
+      this.setState({ complete: '' });
+    } else {
+      this.setState({ complete: 'complete' });
+    }
+  }
+
+  checkBoxOnChangeIncomplete = ({ name, value }) => {
+    if(this.state.incomplete) {
+      this.setState({ incomplete: '' });
+    } else {
+      this.setState({ incomplete: 'incomplete' });
+    }
+  }
+
+  checkBoxOnChangePendingReview = ({ name, value }) => {
+    if(this.state.pendingReview) {
+      this.setState({ pendingReview: '' });
+    } else {
+      this.setState({ pendingReview: 'pending-review' });
+    }
+  }
+
   render() {
     const { error, redirectTo } = this.state;
     if (redirectTo) return <Redirect push to={redirectTo} />;
@@ -68,9 +128,9 @@ export default class HomePage extends Component {
           <div className='content'>
             <div className='data-input-status'>
               <FormInput
-                labelText='Filter on'
-                name='dataInputStatusFilterString'
-                value={this.state.dataInputStatusFilterString}
+                labelText='Search...'
+                name='brandFilterString'
+                value={this.state.brandFilterString}
                 type='text'
                 onChange={this.onChange}
               />
@@ -78,6 +138,29 @@ export default class HomePage extends Component {
             <button className='green-button' onClick={this.goToProductNewPage}>
               <i className='fas fa-plus' /> New Product
             </button>
+            <div style={{ }}>
+              <label> Complete </label>
+              <input
+                type='checkbox'
+                name={productStatusComplete.key}
+                value={productStatusComplete.value}
+                onChange={this.checkBoxOnChangeComplete}
+              />
+              <label> Incomplete </label>
+              <input
+                type='checkbox'
+                name={productStatusIncomplete.key}
+                value={productStatusComplete.value}
+                onChange={this.checkBoxOnChangeIncomplete}
+              />
+              <label> Pending Review </label>
+              <input
+                type='checkbox'
+                name={productStatusPendingReview.key}
+                value={productStatusPendingReview.value}
+                onChange={this.checkBoxOnChangePendingReview}
+              />
+            </div>
           </div>
         </header>
         { this.renderProducts() }
